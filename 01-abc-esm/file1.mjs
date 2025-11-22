8de450e5cc4e5de309b23b1ce2d37d9efef3777b
aashikur <mdaashikur@gmail.com>
2025-11-22 13:05:18 +0600

    alis

--- 01-abc-esm/file1.mjs
+++ 01-abc-esm/file1.mjs
@@ -1,10 +1,10 @@
 
-import {a, b} from './file2.mjs';
+import {a as d, b} from './file2.mjs'; // alisasing 'a' to 'd'
 import {add, amrSub} from './utils/index.mjs'; // the index must be specified in esm
 
 
 
-console.log("test: ", a, b);                  
+console.log("test: ", d, b);                  
 
 console.log("Addition: ", add(a, b));
 console.log("Subtraction: ", amrSub(a, b));
