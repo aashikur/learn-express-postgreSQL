24ffbb3075fa81d90465bd8332d711fab3045ed9
aashikur <mdaashikur@gmail.com>
2025-12-10 09:26:21 +0600

    auth - login

---
+++ part-3-express-postgreSQL-Authentication/src/modules/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { authService } from "./auth.service";
+
+
+
+const loginUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+            const result = await authService.loginUser(email, password);
+            console.log(result);
+            if (!result) {
+                return res.status(401).json({
+                    success: false,
+                    message: 'Invalid email or password',
+                });
+            }
+            res.status(200).json({
+                success: true,
+                message: 'User logged in successfully',
+                data: {
+                    user: result.user,
+                    token: result.token,
+                },
+            });
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Invalid email or password',
+        })
+    }
+}
+
+
+export const authController = {
+    loginUser,
+}
--- part-3-express-postgreSQL-Authentication/src/modules/auth/auth.router.ts
+++ part-3-express-postgreSQL-Authentication/src/modules/auth/auth.router.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { authController } from "./auth.controller";
 
 const router = Router();
 
+// localhost:3000/auth/register
+router.post('/login',authController.loginUser);
 
 
 
